aa0709c3dadfe11e35adfcf502dcaa4b8e53c2b9
EldadCodere <eldad.yikne@codere.com>
2024-12-24 10:50:52 +0200

    add personal to xcel

--- src/components/SoldierXcelDownload.tsx
+++ src/components/SoldierXcelDownload.tsx
@@ -9,9 +9,10 @@ export default function SoldierXcelDownload({ data }: Props) {
     // Insert RTL mark to suggest RTL formatting in Excel
     const rtlMark = "\u200F"; // This is the Right-To-Left Mark (RTLM)
 
-    const rows = data.map((item: any) => [
+    const rows = data.map((item: Soldier) => [
       rtlMark + item.name, // Apply RTL mark to the name
       rtlMark + item.team, // Apply RTL mark to the team
+      item.personalNumber ?? "",
       item.size?.pance ?? "",
       item.size?.shoes ?? "", // Handle empty shoes size
       item.size?.short ?? "",
